a12e4bbbce1d1544e7e520b86ed86264b1583346
Henrique Zani <henrique.zani@hotmail.com>
2022-06-05 20:17:14 -0300

    Login View Controller

---
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Bankey
+//
+//  Created by Zani on 05/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+
+
+}
+
+extension LoginViewController {
+    private func style(){
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout(){
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
